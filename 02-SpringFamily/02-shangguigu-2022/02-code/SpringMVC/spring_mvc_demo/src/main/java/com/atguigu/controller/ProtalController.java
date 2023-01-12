4999485657d88cdeaa23791e9a77353473cb4f65
lm101845 <1018457683@qq.com>
2023-01-12 21:51:06 +0800

    第123-124集看完

---
+++ 02-SpringFamily/02-shangguigu-2022/02-code/SpringMVC/spring_mvc_demo/src/main/java/com/atguigu/controller/ProtalController.java
@@ -0,0 +1,21 @@
+package com.atguigu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author liming
+ * @Date 2023/1/12 21:25
+ **/
+@Controller
+public class ProtalController {
+    @RequestMapping("/")
+    public String protal(){
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "success";
+    }
+}
