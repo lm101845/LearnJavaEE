a13f89bac0104324dc016c5cbb740b6dd7492df3
lm101845 <1018457683@qq.com>
2023-02-19 22:16:31 +0800

    第26-28集看完

---
+++ 03-SpringBoot2/02-code/boot-02-web-01/src/main/java/com/atguigu/boot/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.atguigu.boot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+/**
+ * @Author liming
+ * @Date 2023/2/19 21:23
+ **/
+@Configuration(proxyBeanMethods = false)
+public class WebConfig {
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+        HiddenHttpMethodFilter methodFilter = new HiddenHttpMethodFilter();
+        methodFilter.setMethodParam("_m");
+        return methodFilter;
+    }
+}
--- 03-SpringBoot2/02-code/boot-02-web-01/src/main/java/com/atguigu/boot/controller/HelloController.java
+++ 03-SpringBoot2/02-code/boot-02-web-01/src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.atguigu.boot.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author liming
@@ -23,4 +22,33 @@ public class HelloController {
         //此时会返回字符串aaa还是返回图片呢？
         //答：返回的是aaa字符串！！！
     }
+
+    @GetMapping("/user")
+    //@RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String getUser(){
+
+        return "GET-张三";
+    }
+
+    @PostMapping("/user")
+    //@RequestMapping(value = "/user",method = RequestMethod.POST)
+    public String saveUser(){
+        return "POST-张三";
+    }
+
+
+    @PutMapping("/user")
+    //@RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public String putUser(){
+
+        return "PUT-张三";
+    }
+
+    @DeleteMapping("/user")
+    //@RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String deleteUser(){
+        return "DELETE-张三";
+    }
+
+    //扩展点：如何把 _method 这个名字换成我们自己喜欢的
 }
