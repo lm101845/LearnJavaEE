ad42e010875c0ce829e8d695b307581ad867fb7e
lm101845 <1018457683@qq.com>
2023-07-02 05:41:55 +0800

    第11-15集看完

---
+++ 05-SpringCloud/01-heima/02-code/cloud-demo/eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author liming
+ * @Date 2023/7/2 4:40
+ **/
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
--- 05-SpringCloud/01-heima/02-code/cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ 05-SpringCloud/01-heima/02-code/cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,8 +1,11 @@
 package cn.itcast.order;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,7 +21,13 @@ public class OrderApplication {
      * 创建RestTemplate并注入Spring容器
      */
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+    //@Bean
+    //public IRule randomRule(){
+    //    return new RandomRule();
+    //}
 }
